51a04cd3fc43f9f351bae73b4ed64e915dd551fb
Ana0603 <167488222+Ana0603@users.noreply.github.com>
2026-07-08 12:08:10 -0500

    Create supabase.js to configure Supabase client
    Initialize Supabase client with environment variables.

---
+++ api/supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Faltan las variables de entorno SUPABASE_URL o SUPABASE_ANON_KEY."
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
